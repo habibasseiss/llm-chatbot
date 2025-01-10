f695b662eecb0bc9677331702991a9a2b5ff6b44
Habib Asseiss <habibasseiss@gmail.com>
2025-01-10 13:59:50 -0400

    replace groq with ollama

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { PostgresDatabaseConnection } from "@/infrastructure/database/DatabaseConnection";
-import { GroqAIGateway } from "@/infrastructure/gateways/GroqAIGateway";
 import { HttpAPIGateway } from "@/infrastructure/gateways/HttpAPIGateway";
 import { DatabasePromptRepository } from "@/infrastructure/repositories/DatabasePromptRepository";
 import { ExpressServer } from "@/infrastructure/webserver/ExpressServer";
 import { WebhookController } from "@/interfaces/controllers/WebhookController";
 import { HandleIncomingMessage } from "@/usecases/message/HandleIncomingMessage";
 import dotenv from "dotenv";
+import { OllamaAIGateway } from "./infrastructure/gateways/OllamaAIGateway";
 import { ApiController } from "./interfaces/controllers/ApiController";
 import { HandleChatSession } from "./usecases/message/HandleChatSession";
 
 dotenv.config();
 
 const PORT = 3000;
-const { GRAPH_API_TOKEN, GROQ_API_KEY, DATABASE_URL, API_URL, API_KEY } =
+const { GRAPH_API_TOKEN, OLLAMA_HOST, DATABASE_URL, API_URL, API_KEY } =
   process.env;
 
-const aiGateway = new GroqAIGateway(GROQ_API_KEY!);
+const aiGateway = new OllamaAIGateway(OLLAMA_HOST!);
 const apiGateway = new HttpAPIGateway(API_URL!, API_KEY!);
 const pgDatabaseConnection = new PostgresDatabaseConnection(DATABASE_URL!);
 const promptRepository = new DatabasePromptRepository(pgDatabaseConnection);
